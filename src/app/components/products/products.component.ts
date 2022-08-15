1ee03f3f5bf3a62b43c20c058a8e3147689093a5
meuxdev <meuxdev@gmail.com>
2022-08-15 17:42:29 -0500

    Setup create the product component and service.

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
+  products: Product[] = [];
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
 
+  getAllProducts() {
+    this.productService.getAllSimple().subscribe({
+      next: (products) => {
+        this.products = [...products];
+      },
+    });
+  }
 }
