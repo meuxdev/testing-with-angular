ba9fa1230eaf8ffd40754e717e2bb6acbf3c0f0d
meuxdev <meuxdev@gmail.com>
2022-08-18 17:09:44 -0500

    HttpClient Testing module.

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,15 +1,59 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ProductService } from './product.service';
+import { Product } from '../models/product.model';
+import { environment } from '../../environments/environment';
 
-describe('ProductService', () => {
+fdescribe('ProductService', () => {
   let service: ProductService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      // imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
+      providers: [ProductService],
+    });
     service = TestBed.inject(ProductService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('Test for the GetAllSimple', () => {
+    it('should return a product list', (doneFn) => {
+      // arrange
+      const mockData: Product[] = [
+        {
+          id: '123',
+          title: 'title',
+          description: 'blabla',
+          price: 12,
+          category: {
+            id: 112,
+            name: 'as',
+          },
+          images: ['img', 'img'],
+        },
+      ];
+      // Act
+      service.getAllSimple().subscribe((data) => {
+        // Assert
+        expect(data.length).toEqual(mockData.length);
+        expect(data).toEqual(mockData);
+        doneFn();
+      });
+
+      // http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const request = httpController.expectOne(url);
+      request.flush(mockData);
+      httpController.verify();
+    });
   });
 });
